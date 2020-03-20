1e93743154bcacee412bc93fafb38068d712cee1
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2020-03-20 19:12:39 +0100

    Pretty print known lambda functions

--- src/Lambda/Term.hs
+++ src/Lambda/Term.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE ViewPatterns    #-}
+
 module Lambda.Term where
   --( Term(..)
   --, reduce
@@ -15,6 +18,7 @@ repeatUntilNothing f a = a : maybe [] (repeatUntilNothing f) (f a)
 reduce :: Term -> Maybe Term
 reduce (Apply (Lambda t) b) = Just $ substitute 0 t b
 reduce (Apply a b)          = (Apply <$> reduce a <*> pure b) <|> (Apply a <$> reduce b)
+reduce (Lambda t)           = Lambda <$> reduce t
 reduce _                    = Nothing
 
 substitute :: Int -> Term -> Term -> Term
@@ -50,6 +54,7 @@ termAnd :: Term
 termAnd = Lambda $ Lambda (Apply (Apply (Var 1) (Var 0)) termF)
 
 showTerm :: Int -> Term -> String
+showTerm _ (showFancy -> Just x)   = x
 showTerm n (Var value)             = showVar (n - 1 - value)
 showTerm n (Lambda inner)          = '\\' : showVar n ++ showInnerLambda (n + 1) inner
 showTerm n (Apply l@(Apply _ _) r) = showTerm n l ++ " " ++ showTermMightParen n r
@@ -67,3 +72,46 @@ showVar n
 showInnerLambda :: Int -> Term -> String
 showInnerLambda n (Lambda inner) = " " ++ showVar n ++ showInnerLambda (n + 1) inner
 showInnerLambda n t              = "." ++ showTerm n t
+
+-- Fancy function naming
+
+showFancy :: Term -> Maybe String
+showFancy TermT   = Just "T"
+showFancy TermF   = Just "F"
+showFancy TermAnd = Just "AND"
+showFancy TermId  = Just "ID"
+showFancy _       = Nothing
+
+
+pattern TermT :: Term
+pattern TermT = Lambda2 (Var 1)
+pattern TermF :: Term
+pattern TermF = Lambda2 (Var 0)
+pattern TermAnd :: Term
+pattern TermAnd = Lambda2 (Apply3 (Var 1) (Var 0) (Lambda2 (Var 0)))
+pattern TermId :: Term
+pattern TermId = Lambda (Var 0)
+
+pattern Apply3 :: Term -> Term -> Term -> Term
+pattern Apply3 a b c = Apply (Apply a b) c
+pattern Apply4 :: Term -> Term -> Term -> Term -> Term
+pattern Apply4 a b c d = Apply (Apply3 a b c) d
+pattern Apply5 :: Term -> Term -> Term -> Term -> Term -> Term
+pattern Apply5 a b c d e = Apply (Apply4 a b c d) e
+pattern Apply6 :: Term -> Term -> Term -> Term -> Term -> Term -> Term
+pattern Apply6 a b c d e f = Apply (Apply5 a b c d e) f
+pattern Apply7 :: Term -> Term -> Term -> Term -> Term -> Term -> Term -> Term
+pattern Apply7 a b c d e f g = Apply (Apply6 a b c d e f) g
+
+pattern Lambda2 :: Term -> Term
+pattern Lambda2 a = Lambda (Lambda a)
+pattern Lambda3 :: Term -> Term
+pattern Lambda3 a = Lambda (Lambda2 a)
+pattern Lambda4 :: Term -> Term
+pattern Lambda4 a = Lambda (Lambda3 a)
+pattern Lambda5 :: Term -> Term
+pattern Lambda5 a = Lambda (Lambda4 a)
+pattern Lambda6 :: Term -> Term
+pattern Lambda6 a = Lambda (Lambda5 a)
+pattern Lambda7 :: Term -> Term
+pattern Lambda7 a = Lambda (Lambda6 a)
